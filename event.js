0f6b56b4e4f66ecda1dd00673a99e2408c7cbd25
jorgef2210 <100946488+jorgef2210@users.noreply.github.com>
2023-01-25 17:01:40 -0300

    Add files via upload

---
+++ event.js
@@ -0,0 +1,5 @@
+function colorChanger() {
+    let selectValue = document.getElementById("colors").value;
+    document.getElementById("nom").innerHTML = "EDWARD WALLACE";
+    document.body.style.backgroundColor = selectValue;
+}
